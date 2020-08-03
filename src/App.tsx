579e7964e0da5ce26cf2ecafbd300abbd4218826
Matteo Pieroni <matteo.pieroni@zonedigital.com>
2020-08-03 13:31:48 +0200

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
 import React from 'react';
+import netlify from 'netlify-auth-providers';
+import axios from 'axios';
 
-export const App = (): JSX.Element => <div>Hello world</div>;
+const auth = new netlify({ site_id: '' });
+
+const get = axios.get('ciao');
+
+export const App = (): JSX.Element => (
+	<div>
+		Hello world
+		<button
+			onClick={() => {
+				auth.authenticate({ provider: 'github' }, (err, data) => {
+					console.log(err, data);
+				});
+			}}
+		>
+			login
+		</button>
+	</div>
+);
